1b2e5b4566ef2bb3a52b6811a290e5f6d4c71325
danielvdg <daniel.vdg@hotmail.com>
2021-09-17 20:10:55 -0300

    implementando cifra

--- criptografar.js
+++ criptografar.js
@@ -1,47 +1,111 @@
-var metodoDeCodificacao = document.querySelector('#metodoDeCodificao')
-var selecaoDeMetodo  = document.querySelector('#selectMethod')
+var metodoDeCodificacao = document.querySelector("#metodoDeCodificao");
+var selecaoDeMetodo = document.querySelector("#selectMethod");
 
-var mensagemDeEntrada = document.querySelector('#mensagemEntrada')
-var mensagemDeSaida = document.querySelector('#mensagemSaida')
-var resultadoConteiner = document.querySelector('.resultado')
+var mensagemDeEntrada = document.querySelector("#mensagemEntrada");
+var mensagemDeSaida = document.querySelector("#mensagemSaida");
+var resultadoConteiner = document.querySelector(".resultado");
 
-var radioCodificar = document.querySelector('#radioCodificar')
-var radioDescodificar = document.querySelector('#radioDescodificar')
+var radioCodificar = document.querySelector("#radioCodificar");
+var radioDescodificar = document.querySelector("#radioDescodificar");
+var incremento = document.querySelector(".incremento");
+var incrementoValor = document.querySelector("#incremento");
 
-var btnCodificar  = document.querySelector('#btnCodificar')
-var btnDescodificar = document.querySelector('#btnDescodificar')
 
-selecaoDeMetodo.addEventListener('change', ()=>{
+var btnCodificar = document.querySelector("#btnCodificar");
+var btnDescodificar = document.querySelector("#btnDescodificar");
 
-    if(metodoDeCodificacao.value === 'base64'){
+selecaoDeMetodo.addEventListener("change", () => {
+
+    btnCodificar.classList.add("invisivel");
+    btnDescodificar.classList.add("invisivel");
+    incremento.classList.add("invisivel");
+    resultadoConteiner.classList.add("invisivel");
+
+
+  if (metodoDeCodificacao.value === "base64") {
+    incremento.classList.add("invisivel");
+    radioCodificar.addEventListener("focus", () => {
+      btnCodificar.classList.remove("invisivel");
+      btnDescodificar.classList.add("invisivel");
+      resultadoConteiner.classList.add("invisivel");
+      btnCodificar.addEventListener("click", () => {
+        resultadoConteiner.classList.remove("invisivel");
+        mensagemDeSaida.textContent = btoa(mensagemDeEntrada.value);
+      });
+    });
+
+    radioDescodificar.addEventListener("focus", () => {
+      btnDescodificar.classList.remove("invisivel");
+      btnCodificar.classList.add("invisivel");
+      resultadoConteiner.classList.add("invisivel");
+      btnDescodificar.addEventListener("click", () => {
+        resultadoConteiner.classList.remove("invisivel");
+       mensagemDeSaida.textContent = atob(mensagemDeEntrada.value);
+      });
+    });
+  } else if (metodoDeCodificacao.value === "cifraCs") {
+    incremento.classList.remove("invisivel");
+    radioCodificar.addEventListener("focus", () => {
+      btnCodificar.classList.remove("invisivel");
+      btnDescodificar.classList.add("invisivel");
+      resultadoConteiner.classList.add("invisivel");
+
+
+      btnCodificar.addEventListener("click", () => {
+        resultadoConteiner.classList.remove('invisivel')
+        let arryText = mensagemDeEntrada.value.split('')
+        let numeracaoAscii = []
         
-        radioCodificar.addEventListener('focus', ()=>{
-            btnCodificar.classList.remove('invisivel')
-            btnDescodificar.classList.add('invisivel')
-            btnCodificar.addEventListener('click',()=>{
-                resultadoConteiner.classList.remove('invisivel') 
-                mensagemDeSaida.textContent= btoa(mensagemDeEntrada.value)
-                
-            })
-
-        })
-
-        radioDescodificar.addEventListener('focus', ()=>{
-            btnDescodificar.classList.remove('invisivel')
-            btnCodificar.classList.add('invisivel')
-            btnDescodificar.addEventListener('click',()=>{
-                resultadoConteiner.classList.remove('invisivel') 
-                mensagemDeSaida.textContent= atob(mensagemDeEntrada.value)
-                
-            })
-
-        })
+        let LetrasAscii = []
+        for (let i = 0 ; i < arryText.length ; i++){
+          numeracaoAscii.push(arryText[i].charCodeAt() + parseInt(incrementoValor.value))
+          
+        }
        
-    }else if(metodoDeCodificacao.value ==='cifraCs'){
-        console.log('cifra');
-        
-    }else{
-        console.log('selecione algo');
-    }
+        for (let i = 0 ; i < numeracaoAscii.length; i++ ){
+          LetrasAscii.push(String.fromCharCode(numeracaoAscii[i]))
+          
+        }
 
-})
+        mensagemDeSaida.textContent = LetrasAscii.join('')
+        console.log(LetrasAscii);
+        
+        
+        
+
+        
+       
+
+      });
+    });
+
+    radioDescodificar.addEventListener("focus", () => {
+      btnDescodificar.classList.remove("invisivel");
+      btnCodificar.classList.add("invisivel");
+      resultadoConteiner.classList.add("invisivel");
+
+      btnDescodificar.addEventListener("click", () => {
+        
+        resultadoConteiner.classList.remove('invisivel')
+        let arryText = mensagemDeEntrada.value.split('')
+        let numeracaoAscii = []
+        
+        let LetrasAscii = []
+        for (let i = 0 ; i < arryText.length ; i++){
+          numeracaoAscii.push(arryText[i].charCodeAt() - parseInt(incrementoValor.value))
+          
+        }
+       
+        for (let i = 0 ; i < numeracaoAscii.length; i++ ){
+          LetrasAscii.push(String.fromCharCode(numeracaoAscii[i]))
+          
+        }
+
+        mensagemDeSaida.textContent = LetrasAscii.join('')
+        console.log(LetrasAscii);
+      });
+    });
+  } else {
+    console.log("selecione algo");
+  }
+});
